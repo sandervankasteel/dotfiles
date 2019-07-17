1454ff26417139117de5829a6553a6fd8e6b567d
Sander van Kasteel <info@sandervankasteel.nl>
2019-07-17 22:01:29 +0200

    Stub implementation of the install functions

---
+++ functions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function install_kitty_configuration() {
+    echo "Installing kitty configuration..."
+}
+
+function install_nano_configuration() {
+    echo "Installing nano configuration..."
+}
+
+function install_zsh_configuration() {
+    echo "Installing zsh configuration..."
+}
+
+function install_githooks() {
+    echo "Installing githooks..."
+
+    echo $(pwd)
+}
