3f4a305ac088036df7951554f1d0320227eb6ac4
Sander van Kasteel <info@sandervankasteel.nl>
2019-07-17 22:01:15 +0200

    Expanded the install script to dynamically call the matchin install_app_configuration function

--- install.sh
+++ install.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-echo -n "Do you want to use Githooks apply all changes after fetching the latest sources ? (Y/N) "
-read use_githooks
-use_githooks=$(echo "$use_githooks") | tr '[:upper:]' '[:lower:]' 
+# quite_mode = $@
 
-if [[ $use_githooks == 'y' ]]; then
-    echo -n "Use githooks"
-elif [[ $use_githooks == 'n' ]]; then
-    echo -n "Don't Use githooks"
-else
-    echo -n "Parsing of question failed :("
-    exit -1;
+# Import functions.sh
+source functions.sh
+
+# Determine all the dotfiles that need installation, excluding githooks (since that might not me desirable all the time)
+TO_INSTALL=$(printf '%s\n' */ | sort -r | sed 's:/$::' | sed 's/githooks//')
+
+
+if [ ! -f .installed.lock ]; then
+    echo -n "Do you want to use Githooks apply all changes after fetching the latest sources ? (Y/n) "
+    read use_githooks
+    use_githooks=$(echo "$use_githooks") | tr '[:upper:]' '[:lower:]' 
+
+    if [[ $use_githooks == 'y' ]]; then
+        install_githooks
+    fi
 fi
 
+# Execute the installation
+
+for DIR in ${TO_INSTALL[@]}
+do
+    install_${DIR}_configuration 
+done
+
+# When everything has run perfectly, create a lock file to prevent double installation
 # touch ./.installed.lock
