4b61ba1eebb9c44afe8974fb52f5bd3d7f0a0402
Sander van Kasteel <info@sandervankasteel.nl>
2019-07-17 23:57:43 +0200

    Added some outline for installing the configuration for Kitty

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# quite_mode = $@
-
 # Import functions.sh
 source functions.sh
 
 # Determine all the dotfiles that need installation, excluding githooks (since that might not me desirable all the time)
-TO_INSTALL=$(printf '%s\n' */ | sort -r | sed 's:/$::' | sed 's/githooks//')
+to_install=$(printf '%s\n' */ | sort -r | sed 's:/$::' | sed 's/githooks//')
 
-
+# Only ask if the user, if he/she wants to use Githooks if we aren't installed already
 if [ ! -f .installed.lock ]; then
     echo -n "Do you want to use Githooks apply all changes after fetching the latest sources ? (Y/n) "
     read use_githooks
@@ -19,11 +17,15 @@ if [ ! -f .installed.lock ]; then
     fi
 fi
 
-# Execute the installation
-
-for DIR in ${TO_INSTALL[@]}
+# Execute the installation of the configuration files
+for APPLICATION_NAME in ${to_install[@]}
 do
-    install_${DIR}_configuration 
+    # Only run the install_${APPLICATION_NAME}_configuration function if that function is defined
+    if [ "$(type -t install_${APPLICATION_NAME}_configuration)" = "function" ]; then
+        install_${APPLICATION_NAME}_configuration 
+    else
+        echo "Skipping installation of ${APPLICATION_NAME}'s configuration files because a \`install_${APPLICATION_NAME}_configuration\` function isn't defined"
+    fi
 done
 
 # When everything has run perfectly, create a lock file to prevent double installation
